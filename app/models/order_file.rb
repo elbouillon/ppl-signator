bbf7ad8d1e7fec83a8602d5b0b82759a14157b60
Mickael Kurmann <elbouillon@gmail.com>
2012-02-27 22:20:31 +0100

    translation updates, some ui improvements

--- app/models/order_file.rb
+++ app/models/order_file.rb
@@ -2,20 +2,54 @@
 
 class OrderFile
   include ActiveAttr::Model
+  include ApplicationHelper
 
   SIGNATORS = {
-  "ak" => "André Kurmann",
-  "mk" => "Mickael Kurmann"
+  "André Kurmann" => "André Kurmann",
+  "Mickael Kurmann" => "Mickael Kurmann"
   }
 
-  attribute :file
   attribute :signator
+  attribute :delivery_date
 
-  attr_accessible :file, :signator
+  def to_s
+    "#{@original_filename} - #{@content_type}"
+  end
 
-  validates_presence_of :file, :signator
+  def render_file
+    Prawn::Document.new(:template => @tempfile) do |pdf|
+      require "prawn/measurement_extensions"
 
-  def save?
-    true
+      pdf.go_to_page(pdf.page_count)
+      pdf.font_size 16
+
+      pdf.move_down 20.cm
+
+      pdf.text "Bon pour accord de fabrication"
+      pdf.text "Panorama Profil line SA"
+      pdf.text "La Sarraz, le #{I18n.l(Date.today, format: :long)} - #{signator}"
+
+      pdf.move_down 1.cm
+
+      pdf.fill_color "D3482D"
+      pdf.font_size(20){
+        pdf.text "Date de livraison : #{return_display_name_from_date(delivery_date.to_date)}"
+      }
+    end.render
+  end
+
+  def upload=(upload_file)
+    @original_filename = upload_file.original_filename
+    @content_type= upload_file.content_type
+    @tempfile= upload_file.tempfile
+  end
+
+  def name
+    @original_filename.to_s[0..-5]
+  end
+
+  def filename
+    "confirmation_#{signator}_#{name}".parameterize + ".pdf"
   end
 end
+
