be94d3c21d19676d6f192dbf4aa72a54a835e65c
Mickael Kurmann <mickael.kurmann@gmail.com>
2019-02-02 21:10:31 +0100

    fixed encoding bug

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
+Encoding.default_external = Encoding::UTF_8
+# Encoding.default_internal = Encoding::UTF_8
+
 require(File.expand_path('trailblazer-config', File.dirname(__FILE__)))
 require_relative 'signator'
 require 'roda'
